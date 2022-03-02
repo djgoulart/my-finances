ef83cca6e9a286ecb473e902ead21f5fc2a78132
Diego <diegogoulart.aws@gmail.com>
2022-03-02 19:51:06 -0300

    feat:(register) Add form input

--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
-
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Header = styled.View`
@@ -20,4 +21,11 @@ export const Title = styled.Text`
   color: ${({ theme }) => theme.colors.shape};
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(18)}px;
+`;
+
+export const Form = styled.View`
+  flex: 1;
+  width: 100%;
+
+  padding: ${RFValue(24)}px;
 `;
