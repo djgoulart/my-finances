9a9ff01473028a704b28911f7b443dc8dc47fa3c
Diego <diegogoulart.aws@gmail.com>
2022-03-04 13:43:58 -0300

    feat:(button) Change category select button

--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -1,7 +1,7 @@
 import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 import { Feather } from '@expo/vector-icons';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { GestureHandlerRootView, RectButton } from 'react-native-gesture-handler';
 
 interface CategoryProps {
   isActive: boolean;
@@ -29,7 +29,7 @@ export const Title = styled.Text`
   font-size: ${RFValue(18)}px;
 `;
 
-export const Category = styled.TouchableOpacity<CategoryProps>`
+export const Category = styled(RectButton) <CategoryProps>`
   width: 100%;
   padding: ${RFValue(15)}px;
 
@@ -37,7 +37,7 @@ export const Category = styled.TouchableOpacity<CategoryProps>`
   align-items: center;
 
   background-color: ${({ isActive, theme }) =>
-    isActive ? theme.colors.secondary_light : theme.colors.background
+    isActive ? theme.colors.secondary_light : theme.colors.shape
   };
 `;
 
